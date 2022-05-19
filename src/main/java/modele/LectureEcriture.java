dbd3e0975afe20cbd3dd239f709b9a9bd1ca706d
Yanis Merabet <yanismerabet16@gmail.com>
2022-05-19 18:24:25 +0200

    Avancement de la méthode lectureDistances
    A faire : implémentation des composants dans VBoxRoot et finalisation de la méthode lectureDistances

--- src/main/java/modele/LectureEcriture.java
+++ src/main/java/modele/LectureEcriture.java
@@ -3,9 +3,9 @@ package modele;
 import java.io.*;
 import java.util.*;
 
-public class LectureEcriture implements ConstantesDistances{
+public class LectureEcriture implements ConstantesVille {
     public static HashMap<String,Integer> ville = new HashMap<>();
-
+    public static int [][] tabDistances = new int [28][28];
     public static Scenario lectureScenario (File fichier) throws IOException{
         Scenario scenario = new Scenario();
 
@@ -50,18 +50,18 @@ public class LectureEcriture implements ConstantesDistances{
     /*
     public static int [][] lectureDistances(File fichier) throws IOException{
         BufferedReader bufferEntree = new BufferedReader(new FileReader(fichier));
+        LineNumberReader readerLigne = new LineNumberReader(bufferEntree);
         String ligne;
 
         StringTokenizer tokenizer;
         do{
-            for (int i = 0 ; i < 30 ; i++){
-                for (int j = 0 ; i < 30 ; i++){
-                    ligne = bufferEntree.readLine();
-                    if (ligne != null){
-                        tokenizer = new StringTokenizer(ligne, " ");
-                        String tokenDistances = tokenizer.nextToken();
-                        tabDistances[i][j] = Integer.parseInt(tokenDistances);
-                    }
+            for (int i = 0 ; i < 29 ; i++){
+                ligne = readerLigne.readLine();
+                int numeroLigne = readerLigne.getLineNumber() -1;
+                if (ligne != null){
+                    tokenizer = new StringTokenizer(ligne, " ");
+                    String tokenDistances = tokenizer.nextToken();
+                    tabDistances[numeroLigne][i] = Integer.parseInt(tokenDistances);
                 }
             }
         }
@@ -69,10 +69,11 @@ public class LectureEcriture implements ConstantesDistances{
         bufferEntree.close();
         return tabDistances;
     }
-*/
+    */
     public static int getDistance(String ville1, String ville2){
         return TAB_DISTANCES[ville.get(ville1)][ville.get(ville2)];
     }
+
     public static void ecritureScenario (String nomFichier, Scenario scenario) throws IOException{
         PrintWriter sortie = new PrintWriter(new BufferedWriter(new FileWriter(nomFichier)));
         int i = 0;
