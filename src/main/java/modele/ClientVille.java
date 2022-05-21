16563de79fcf9c08fe6e0f50009497294abffe45
Yanis Merabet <yanismerabet16@gmail.com>
2022-05-21 11:08:16 +0200

    Création des Javadoc + création d'un fichier distancesVille.txt

--- src/main/java/modele/ClientVille.java
+++ src/main/java/modele/ClientVille.java
@@ -2,6 +2,7 @@ package modele;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 import static modele.LectureEcriture.*;
 
@@ -9,8 +10,9 @@ import static modele.LectureEcriture.*;
 public class ClientVille {
     public static void main(String [] args) throws IOException {
         File villes = new File("src/main/java/ressources/distances.txt");
+        File distancesVille = new File("src/main/java/ressources/distancesville.txt");
         System.out.println(lectureVille(villes));
-
-        System.out.println(getDistance("Grenoble", "Velizy"));
+        //System.out.println(Arrays.deepToString(lectureDistances(distancesVille)));
+        System.out.println(getDistance("Velizy", "Velizy"));
     }
 }
